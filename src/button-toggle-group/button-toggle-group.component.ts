670324d5262756d838851f3b889f04203d0510d6
Двойкин Алексей <94dvoikin@gmail.com>
2018-02-26 15:58:37 +0300

    [toggle-button-group] update

--- src/button-toggle-group/button-toggle-group.component.ts
+++ src/button-toggle-group/button-toggle-group.component.ts
@@ -3,15 +3,9 @@ import { MatButtonToggle } from '@angular/material';
 
 @Component({
 	selector: 'pip-button-toggle-group',
-	templateUrl: 'button-toggle-group.component.html',
+	template: '<ng-content></ng-content>',
 	styleUrls: ['./button-toggle-group.component.scss']
 })
 export class PipButtonToggleGroupComponent implements OnInit {
-	@Input() vertical: boolean;
-	@Input() name: string;
-	@Input() selected: MatButtonToggle | null;
-	@Input() value: any;
-	@Output() change = new EventEmitter();
-
 	ngOnInit() { }
 }
